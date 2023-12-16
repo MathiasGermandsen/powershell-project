1859773129140b338a449ec2af6538b64ec9805c
mathias <mathias.germandsen@bestseller.com>
2023-12-16 16:22:06 +0100

    updatye

--- PowershellScript.ps1
+++ PowershellScript.ps1
@@ -2,4 +2,13 @@ $desktopPath = [System.IO.Path]::Combine($env:USERPROFILE, 'Desktop')
 $destinationPath = [System.IO.Path]::Combine($desktopPath, 'putty.exe')
 $puttyUrl = "https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe"
 Invoke-WebRequest -Uri $puttyUrl -OutFile $destinationPath
-Write-Host (if (Test-Path $destinationPath) {"Putty.exe downloaded successfully to $destinationPath"} else {"Failed to download Putty.exe"})
+
+# Check if the file was downloaded successfully
+if (Test-Path $destinationPath) {
+    $message = "Putty.exe downloaded successfully to $destinationPath"
+} else {
+    $message = "Failed to download Putty.exe"
+}
+
+# Print the message
+Write-Host $message
